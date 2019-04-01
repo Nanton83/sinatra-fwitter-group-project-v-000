8a613e388b5f7e11fff953adc93640f64da43bd9
Jessica Romans <RomansHomeComputer@Jessicas-iMac.local>
2019-04-01 13:58:40 -0400

    user show page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,5 +18,15 @@ class TweetsController < ApplicationController
         end
     end 
 
+    get '/tweets/:id' do
+        if logged_in?
+            @tweets = @current_user.tweets
+        erb :'user/show'
+        binding.pry
+        else
+            redirect to "/login"
+        end
+    end
+
 
 end
