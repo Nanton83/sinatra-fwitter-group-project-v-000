aac31ec4da5384ed7eab1a6b6c96606adb8a4507
Jessica Romans <RomansHomeComputer@Jessicas-iMac.local>
2019-04-01 14:58:18 -0400

    worked with new tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,7 @@ class TweetsController < ApplicationController
 
     get '/tweets' do
         if logged_in?
+        @tweets = Tweet.all
         erb :'tweets/tweets'
         else
             redirect to "/login"
@@ -16,13 +17,24 @@ class TweetsController < ApplicationController
         else
             redirect to "/login"
         end
-    end 
+    end
+
+    post '/tweets' do
+        if logged_in? && params[:content] == ""
+            redirect to '/tweets/new'
+        elsif logged_in?
+        @tweet = params[:content]
+        redirect to '/tweets/tweets'
+        else
+        redirect to "/login"
+        end
+    end
+ 
 
     get '/tweets/:id' do
         if logged_in?
-            @tweets = @current_user.tweets
-        erb :'user/show'
-        binding.pry
+            @tweet = Tweet.find_by_id(params[:id])
+        erb :'tweets/show_tweet'
         else
             redirect to "/login"
         end
